33e6a59369704288f418a19af23e31d8222714c0
softdeveloper23 <brannongarrett77@gmail.com>
2025-07-03 13:51:27 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from turtle import Screen
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+
+
+
+
+
+screen.exitonclick()
